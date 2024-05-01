8397576ca3d4baa793640cc88b6c41f80259f9c7
garvit <rw3.garvit.rd@gmail.com>
2024-05-01 09:08:51 +0530

    formdata

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef, useState } from "react";
+import axios from "axios";
 
 function App() {
+  let [file, setFile] = useState();
+  let partyName = useRef();
+
+  let handle = (e) => {
+    e.preventDefault();
+
+    setFile(e.target.files[0]);
+  };
+
+  let submit = () => {
+    console.log(file);
+
+    // let data = {
+    //   partyName: partyName.current.value,
+    //   logo: file,
+    // };
+
+    let formdata = new FormData();
+    formdata.append("partyName", partyName.current.value);
+    formdata.append("logo", file);
+
+    let option = {
+      header: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+
+    axios.post("localhost:3001/posts",  {formdata, option});
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" ref={partyName} />
+      <input type="file" onChange={handle} />
+      <input type="submit" onClick={submit} />
     </div>
   );
 }
